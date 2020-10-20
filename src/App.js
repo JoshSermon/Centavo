8a6f4106f8b3582eab64a4e44d96958843d1f93d
JJ Sermon <thejoshuasermon@gmail.com>
2020-10-19 21:32:05 -0400

    Navbar Elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>What's Good Bro!</h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
